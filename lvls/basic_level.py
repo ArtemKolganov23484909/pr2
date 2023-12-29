724e12f3ad8a21f82c99f5b521e0586209ddb0d0
Artem Kolganov <Kolganovar123@yandex.ru>
2023-12-30 01:00:27 +0300

    начальный комит

---
+++ lvls/basic_level.py
@@ -0,0 +1,9 @@
+def importt(path):
+    if path == '1':
+        from codes import lvl1 as lvl
+    elif path == '2':
+        from codes import lvl2 as lvl
+    elif path == '3':
+        from codes import lvl3 as lvl
+    elif path == '4':
+        from codes import lvl4 as lvl
